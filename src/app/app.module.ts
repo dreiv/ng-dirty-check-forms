26b2e66781b81c26703134c119a3477a59d27136
Andrei Voicu <dreivoicu@gmail.com>
2020-07-13 22:23:38 +0300

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SettingsComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/store.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService {
+  private data: BehaviorSubject<any>;
+  data$: Observable<any>;
+
+  constructor() {
+    this.data = new BehaviorSubject({
+      settingOne: 'Initial Value',
+      settingTwo: 'jack',
+      settingThree: true
+    });
+
+    this.data$ = this.data.asObservable();
+  }
+}
