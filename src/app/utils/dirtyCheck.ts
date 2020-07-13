26e6c86eb2211e3cf2263506e7029d2e16881dd7
Andrei Voicu <dreivoicu@gmail.com>
2020-07-13 23:18:17 +0300

    save

--- src/app/utils/dirtyCheck.ts
+++ src/app/utils/dirtyCheck.ts
@@ -1,16 +1,28 @@
-import { Observable, combineLatest } from 'rxjs';
-import { map, debounceTime, shareReplay } from 'rxjs/operators';
-
-const isEqual = (a: any, b: any) =>
-  Object.entries(a).sort().toString() === Object.entries(b).sort().toString();
+import { Observable, combineLatest, fromEvent, Subscription } from 'rxjs';
+import {
+  map,
+  debounceTime,
+  shareReplay,
+  finalize,
+  startWith
+} from 'rxjs/operators';
 
 export function dirtyCheck<U>(source: Observable<U>) {
+  let subscription: Subscription;
+  const isDirty = false;
+
   return <T>(valueChanges: Observable<T>): Observable<boolean> => {
     const isDirty$ = combineLatest(source, valueChanges).pipe(
       debounceTime(300),
-      map(([a, b]) => isEqual(a, b) === false),
+      map(([a, b]) => !Object.is(a, b)),
+      finalize(() => subscription.unsubscribe()),
+      startWith(false),
       shareReplay({ bufferSize: 1, refCount: true })
     );
+
+    subscription = fromEvent(window, 'beforeunload').subscribe((event: any) => {
+      isDirty && (event.returnValue = false);
+    });
 
     return isDirty$;
   };
